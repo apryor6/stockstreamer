cbe06d0a0045f932098c24d4198e8d7ffa61925b
AJ Pryor <apryor6@gmail.com>
2017-09-23 08:11:50 -0400

    date formatting fixed

--- data_plotter.py
+++ data_plotter.py
@@ -1,6 +1,6 @@
 from bokeh.plotting import figure, curdoc, show
 from bokeh.models.sources import ColumnDataSource
-from bokeh.models import Range1d, Legend, NumeralTickFormatter
+from bokeh.models import Range1d, Legend, NumeralTickFormatter, DatetimeTickFormatter
 from bokeh.models.tools import PanTool, BoxZoomTool, WheelZoomTool, ResetTool
 from bokeh.layouts import row
 from bokeh.palettes import Dark2
@@ -11,7 +11,12 @@ import pandas as pd
 import numpy as np
 
 tools = [PanTool(), BoxZoomTool(), ResetTool(), WheelZoomTool()]
-p = figure(title="STOCKSTREAMER", tools=tools, plot_width=1000, y_range=Range1d(-50, 1100), plot_height=680,toolbar_location='below', toolbar_sticky=False)
+
+time_today = (datetime.datetime.today()-datetime.datetime(1970,1,1)).total_seconds()
+time_now = (datetime.datetime.today()-datetime.datetime(1970,1,1)).total_seconds()
+p = figure(title="STOCKSTREAMER v0.0", tools=tools, plot_width=1000,
+ y_range=Range1d(-50, 1100), x_range=Range1d(time_today, time_now),
+ plot_height=680,toolbar_location='below', toolbar_sticky=False)
 # p_imgs = figure(plot_width=512, plot_height=p.plot_height, y_range=p.y_range, toolbar_location=None)
 # p_imgs = figure(plot_width=512, plot_height=680 ,toolbar_location=None)
 
@@ -19,6 +24,7 @@ p = figure(title="STOCKSTREAMER", tools=tools, plot_width=1000, y_range=Range1d(
 p.yaxis.axis_label = "Price ($US)"
 p.yaxis.axis_label_text_font_size = '12pt'
 p.yaxis[0].formatter = NumeralTickFormatter(format="$0")
+p.xaxis[0].formatter = DatetimeTickFormatter()
 
 # p = figure(title="STOCKSTREAMER")
 p.background_fill_color = "#F0F0F0"
@@ -49,12 +55,13 @@ def get_data():
 	AND time >= NOW() - '7 day'::INTERVAL
 	""", conn)
 
+	df['time_s'] = df['time'].apply(lambda x: (x-datetime.datetime(1970,1,1)).total_seconds())
 	grouped = df.groupby('stock_name')
 	unique_names = df.stock_name.unique()
 	ys = [grouped.get_group(stock)['price'] for stock in unique_names]
-	xs = [list(range(len(y))) for y in ys]
+	# xs = [list(range(len(y))) for y in ys]
 
-	# xs = [grouped.get_group(stock)['time'] for stock in unique_names]
+	xs = [grouped.get_group(stock)['time'] for stock in unique_names]
 	max_ys = [np.max(y) for y in ys]
 	return (xs, ys, max_ys, unique_names)
 
@@ -80,47 +87,48 @@ for i, (x, y, max_y, name) in enumerate(zip(xs, ys, max_ys, unique_names)):
 	    line_width=1))
 	    # legend=name))
 	
-	source = ColumnDataSource(dict(y=[max_y],
-								   left=[x[0]],
-			                       right=[x[-1]],
-			                       height=[50],
-			                       fill_alpha=[0.1],
-			                       fill_color=[line_colors[i]],
-			                       line_color=[line_colors[i]]))
-	recs.append(p.hbar(y='y', left='left', right='right', height='height', fill_alpha='fill_alpha',fill_color='fill_color',
-		line_alpha=0.01, line_color='line_color', line_dash='solid', line_width=0.1, source=source))
+	# source = ColumnDataSource(dict(y=[max_y],
+	# 							   left=[x[0]],
+	# 		                       right=[x[-1]],
+	# 		                       height=[50],
+	# 		                       fill_alpha=[0.1],
+	# 		                       fill_color=[line_colors[i]],
+	# 		                       line_color=[line_colors[i]]))
+	# recs.append(p.hbar(y='y', left='left', right='right', height='height', fill_alpha='fill_alpha',fill_color='fill_color',
+	# 	line_alpha=0.01, line_color='line_color', line_dash='solid', line_width=0.1, source=source))
 
 legend = Legend(items=[(stock, [l]) for stock, l in zip(unique_names, lines)], location=(0,0), orientation='horizontal')
 N = len(image_urls)
 latest_timestamp = np.max(xs[0])
-source = ColumnDataSource(dict(
-    url = [image_urls[name] for name in unique_names],
-    x1  = [-128]*N,
-    y1  = max_ys,
-    w1  = [64]*N,
-    h1  = [64]*N,
-))
+# source = ColumnDataSource(dict(
+#     url = [image_urls[name] for name in unique_names],
+#     x1  = [-128]*N,
+#     y1  = max_ys,
+#     w1  = [64]*N,
+#     h1  = [64]*N,
+# ))
 
-p.x_range=Range1d(-256, xs[0][-1])
-image_plot = p.image_url(url='url' ,x='x1', y='y1', w='w1', h='h1',source=source,
- anchor="center", global_alpha=0.7, w_units='screen', h_units='screen')
-# image_plot = p_imgs.image_url(url='url' ,x='x1', y='y1', w='w1', h='h1',source=source, anchor="center", global_alpha=0.7)
+# p.x_range=Range1d(-256, xs[0][-1])
+# image_plot = p.image_url(url='url' ,x='x1', y='y1', w='w1', h='h1',source=source,
+#  anchor="center", global_alpha=0.7, w_units='screen', h_units='screen')
 
 
-def callback():
+def update_figure():
 	xs, ys, max_ys, unique_names = get_data()
+	# p.x_range.update(start=0, stop=100)
+	p.x_range.start=np.min(xs[0])
+	p.x_range.end=np.max(xs[0])
+	# p.x_range=Range1d(xs[0][0], (datetime.datetime.now()-datetime.datetime(1970,1,1)).total_seconds())
 	for i, (x, y, max_y) in enumerate(zip(xs, ys, max_ys)):
 		new_data = dict(x=x, y=y)
 		ds_lines = lines[i].data_source
 		ds_lines.data = new_data
 		ds_circle = circles[i].data_source
 		ds_circle.data = new_data
-		recs[i].data_source.data.update(left=[x[0]], right=[x[-1]])
-		# p_imgs.y_range = p.y_range
+		# recs[i].data_source.data.update(left=[x[0]], right=[x[-1]])
 
-
-
+update_figure()
 p.add_layout(legend, 'below')
 curdoc().add_root(p)
 # curdoc().add_root(row(p, p_imgs))
-curdoc().add_periodic_callback(callback, 5000)
+curdoc().add_periodic_callback(update_figure, 5000)
