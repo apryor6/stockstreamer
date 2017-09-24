0adb99c8b32a5035964520b98ca1bb678cd95412
Ubuntu <ubuntu@ip-172-31-38-173.us-east-2.compute.internal>
2017-09-24 14:03:15 +0000

    working on writeup

--- master.sh
+++ master.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python data_fetcher.py &
-bokeh serve data_plotter.py  --allow-websocket-origin=13.59.119.51:5006
-nohup bokeh serve data_plotter.py  --allow-websocket-origin=13.59.119.51:5006 </dev/null >/dev/null 2>&1 &
+#bokeh serve data_plotter.py  --allow-websocket-origin=13.59.119.51:5006
+nohup bokeh serve stockstreamer.py  --allow-websocket-origin=13.59.119.51:5006 </dev/null >/dev/null 2>&1 &
